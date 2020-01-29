e0000d95e56205678a93dd0b9ee96afe7ed9a016
Alex <sankai1917@gmail.com>
2020-01-29 20:07:11 +0300

    reworked

--- src/jqtask.js
+++ src/jqtask.js
@@ -2,32 +2,31 @@ import $ from 'jquery';
 
 // const xhr = new XMLHttpRequest();
 
+function getContributors(owner, repo) {
+  return $.ajax({
+    dataType: 'json',
+    type: 'GET',
+    url: `https://api.github.com/repos/${owner}/${repo}/contributors`,
+  });
+}
+
 $(document).ready(() => {
   $('#inputButton').click(() => {
     $('.contributors_list').empty();
-    const str = $('#inputText')
+    const requestParams = $('#inputText')
       .val()
       .split('/');
-    // xhr.open(
-    //   "GET",
-    //   `https://api.github.com/repos/${str[0]}/${str[1]}/contributors`,
-    //   false
-    // );
-
-    // xhr.send();
-    $.ajax({
-      dataType: 'json',
-      type: 'GET',
-      url: `https://api.github.com/repos/${str[0]}/${str[1]}/contributors`,
-      success(data) {
-        for (let i = 0; i < data.length; i += 1) {
-          $('.contributors_list').append(
-            `<span class = "item"><a href = "${data[i].html_url}">${i + 1}: ${
-              data[i].login
-            }</a></span>`,
-          );
-        }
-      },
-    });
+    getContributors(requestParams[0], requestParams[1]) .then((content) => {
+      for (let i = 0; i < content.length; i += 1) {
+        $('.contributors_list').append(
+          `<span class = "item"><a href = "${content[i].html_url}">${i + 1}: ${
+            content[i].login
+          }</a></span>`,
+        );
+      }
+    }),
+    (errData) => {
+      console.log(`${errData.status} ${errData.statusText}`);
+    };
   });
 });
