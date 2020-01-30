f40a1c8d78c40f21bccca13287f997c3c5162ac3
Alex <sankai1917@gmail.com>
2020-01-30 13:50:49 +0300

    modified list

--- src/jqtask.js
+++ src/jqtask.js
@@ -2,6 +2,13 @@ import $ from 'jquery';
 
 // const xhr = new XMLHttpRequest();
 
+const goldTier = 25;
+const silverTIer = 15;
+const bronzeTier = 5;
+const goldList = [];
+const silverList = [];
+const bronzeList = [];
+const list = [];
 function getContributors(owner, repo) {
   return $.ajax({
     dataType: 'json',
@@ -12,21 +19,49 @@ function getContributors(owner, repo) {
 
 $(document).ready(() => {
   $('#inputButton').click(() => {
-    $('.contributors_list').empty();
+    $('.list_content').empty();
     const requestParams = $('#inputText')
       .val()
       .split('/');
-    getContributors(requestParams[0], requestParams[1]) .then((content) => {
-      for (let i = 0; i < content.length; i += 1) {
-        $('.contributors_list').append(
-          `<span class = "item"><a href = "${content[i].html_url}">${i + 1}: ${
-            content[i].login
-          }</a></span>`,
-        );
-      }
+    getContributors(requestParams[0], requestParams[1]).then((content) => {
+      content.forEach((element) => {
+        console.log('test');
+        list.push(element);
+        if (element.contributions >= bronzeTier && element.contributions < silverTIer) {
+          bronzeList.push(element);
+        } else if (element.contributions >= silverTIer && element.contributions < goldTier) {
+          silverList.push(element);
+        } else if (element.contributions >= goldTier) {
+          goldList.push(element);
+        }
+      });
     }),
     (errData) => {
       console.log(`${errData.status} ${errData.statusText}`);
     };
+  });
+  $('.all-tier').click(() => {
+    $('.list_content').empty();
+    list.forEach((element) => {
+      $('.list_content').append(`<span class = "list_item">${element.login}</span>`);
+    });
+  });
+  $('.gold-tier').click(() => {
+    $('.list_content').empty();
+    goldList.forEach((element) => {
+      $('.list_content').append(`<span class = "list_item">${element.login}</span>`);
+    });
+  });
+  $('.silver-tier').click(() => {
+    $('.list_content').empty();
+    silverList.forEach((element) => {
+      $('.list_content').append(`<span class = "list_item">${element.login}</span>`);
+    });
+  });
+  $('.bronze-tier').click(() => {
+    $('.list_content').empty();
+    bronzeList.forEach((element) => {
+      $('.list_content').append(`<span class = "list_item">${element.login}</span>`);
+    });
   });
 });
