7a4b972a95c233d3ca150a0dd0adeac3014261e9
Alex <sankai1917@gmail.com>
2020-03-16 18:49:54 +0300

    reworked for knockout js

--- src/jqtask.js
+++ src/jqtask.js
@@ -1,14 +1,30 @@
 import $ from 'jquery';
+import ko from 'knockout';
 
 const silverTIer = 15;
 const bronzeTier = 5;
-let goldList = [];
-let silverList = [];
-let bronzeList = [];
-let list = [];
-let userArr = {};
+const contributors = ko.observableArray([]);
+const userModal = {
+  login: ko.observable(),
+  fname: ko.observable(),
+  sname: ko.observable(),
+  email: ko.observable(),
+  job: ko.observable(),
+  number: ko.observable(),
+};
+let statusValid = false;
 const fieldsArr = ['#firstName', '#lastName', '#jobRole', '#phone'];
-let statusValid;
+
+function sortList(dir) {
+  contributors.sort((left, right) => (left.login.toUpperCase() == right.login.toUpperCase()
+    ? 0
+    : left.login.toUpperCase() < right.login.toUpperCase()
+      ? -1
+      : 1));
+  if (dir === 'DESC') {
+    contributors.reverse();
+  }
+}
 
 function getContributors(owner, repo) {
   return $.ajax({
@@ -26,105 +42,30 @@ function getUserInfo(user) {
   });
 }
 
-function sort(array, type) {
-  if (type === 'asc') {
-    array.forEach((arr) => {
-      for (let i = 0, endI = arr.length - 1; i < endI; i += 1) {
-        for (let j = 0, endJ = endI - i; j < endJ; j += 1) {
-          if (arr[j].login[0].toLowerCase() < arr[j + 1].login[0].toLowerCase()) {
-            const swap = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = swap;
-          }
-        }
+function showGroups(group) {
+  if (group === 'all') {
+    $('.list_item').removeClass('d-none');
+  } else if (group === 'gold') {
+    $('.list_item').each((index, element) => {
+      $(element).addClass('d-none');
+      if ($(element).attr('data-contr') > silverTIer) {
+        $(element).removeClass('d-none');
       }
     });
-  } else {
-    array.forEach((arr) => {
-      for (let i = 0, endI = arr.length - 1; i < endI; i += 1) {
-        for (let j = 0, endJ = endI - i; j < endJ; j += 1) {
-          if (arr[j].login[0].toLowerCase() > arr[j + 1].login[0].toLowerCase()) {
-            const swap = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = swap;
-          }
-        }
+  } else if (group === 'silver') {
+    $('.list_item').each((index, element) => {
+      $(element).addClass('d-none');
+      if ($(element).attr('data-contr') > bronzeTier) {
+        $(element).removeClass('d-none');
       }
     });
-  }
-}
-
-function contrSort(array, type) {
-  if (type === 'asc') {
-    array.forEach((arr) => {
-      for (let i = 0, endI = arr.length - 1; i < endI; i += 1) {
-        for (let j = 0, endJ = endI - i; j < endJ; j += 1) {
-          if (arr[j].contributions > arr[j + 1].contributions) {
-            const swap = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = swap;
-          }
-        }
+  } else if (group === 'bronze') {
+    $('.list_item').each((index, element) => {
+      $(element).addClass('d-none');
+      if ($(element).attr('data-contr') < bronzeTier) {
+        $(element).removeClass('d-none');
       }
     });
-  } else {
-    array.forEach((arr) => {
-      for (let i = 0, endI = arr.length - 1; i < endI; i += 1) {
-        for (let j = 0, endJ = endI - i; j < endJ; j += 1) {
-          if (arr[j].contributions < arr[j + 1].contributions) {
-            const swap = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = swap;
-          }
-        }
-      }
-    });
-  }
-}
-function replaceList(arr) {
-  $('.list_content').empty();
-  arr.forEach((element) => {
-    getUserInfo(element.login).then(
-      (content) => {
-        userArr[content.login] = content;
-        $('.list_content').append(
-          `<div class = "list_item"><span class = "item_login"><a href = "${element.html_url}">${element.login}</a></span>
-        <br>
-        <span class = "item_company">${content.company}</span>
-        <br>
-        <span class = "item_email">${content.email}</span>
-        <br>
-        <span class = "item_location">${content.location}</span>
-        <br>
-        <input type = "button" id = "editProfile" class = "click-me" value = "edit profile" data-user = "${element.login}">
-        </div>`,
-        );
-      },
-      (errData) => {
-        console.log(errData);
-      },
-    );
-  });
-}
-
-function sortAndReplace() {
-  if ($('.sort-type:selected').attr('data-val') === 'alph_a') {
-    sort([list, goldList, bronzeList, silverList], 'asc');
-  } else if ($('.sort-type:selected').attr('data-val') === 'alph_d') {
-    sort([list, goldList, bronzeList, silverList], 'desc');
-  } else if ($('.sort-type:selected').attr('data-val') === 'contr_a') {
-    contrSort([list, goldList, bronzeList, silverList], 'asc');
-  } else if ($('.sort-type:selected').attr('data-val') === 'contr_d') {
-    contrSort([list, goldList, bronzeList, silverList], 'desc');
-  }
-  if ($('.list_content').attr('data-current') === 'all') {
-    replaceList(list);
-  } else if ($('.list_content').attr('data-current') === 'gold') {
-    replaceList(goldList);
-  } else if ($('.list_content').attr('data-current') === 'silver') {
-    replaceList(silverList);
-  } else if ($('.list_content').attr('data-current') === 'bronze') {
-    replaceList(bronzeList);
   }
 }
 
@@ -141,80 +82,61 @@ function validation() {
 }
 
 function handleModal(user) {
-  $('.modal').css('display', 'flex');
   $('.page').css('filter', 'blur(8px)');
-  $('#accName').replaceWith(
-    `<span class="modal-subtitle" id="accName">${userArr[user].login}</span>`,
-  );
-  $('#email').replaceWith(`<span class="modal-subtitle" id="email">${userArr[user].email}</span>`);
-}
-
-$(document).ready(() => {
-  $.ajax({
-    url: 'https://api.github.com/rate_limit',
-  }).then(
+  $('.modal').removeClass('d-none');
+  getUserInfo(user).then(
     (data) => {
-      console.log(data);
+      userModal.login(data.login);
+      userModal.sname(data.name.split(' '[1]));
+      userModal.fname(data.name.split(' ')[0]);
+      userModal.job(data.company);
+      userModal.number(data.id);
+      userModal.email(data.email);
     },
     (errData) => {
       console.log(errData);
     },
   );
-  $('#inputButton').click(() => {
-    userArr = {};
-    list = [];
-    goldList = [];
-    bronzeList = [];
-    silverList = [];
-    $('.list_content').empty();
-    const requestParams = $('#inputText')
+}
+
+$('#inputButton').click(() => {
+  getContributors(
+    $('#inputText')
       .val()
-      .split('/');
-    getContributors(requestParams[0], requestParams[1]).then((content) => {
-      content.forEach((element) => {
-        list.push(element);
-        if (element.contributions <= bronzeTier) {
-          bronzeList.push(element);
-        } else if (element.contributions <= silverTIer) {
-          silverList.push(element);
-        } else {
-          goldList.push(element);
-        }
+      .split('/')[0],
+    $('#inputText')
+      .val()
+      .split('/')[1],
+  ).then(
+    (data) => {
+      data.forEach((element) => {
+        contributors.push(element);
       });
-      $('.all-tier').trigger('click');
-    }),
+      ko.applyBindings({ modal: userModal, contributors });
+    },
     (errData) => {
-      console.log(`${errData.status} ${errData.statusText}`);
-    };
-  });
-  $('.all-tier').click(() => {
-    $('.list_content').attr('data-current', 'all');
-    replaceList(list);
-  });
-  $('.gold-tier').click(() => {
-    $('.list_content').attr('data-current', 'gold');
-    replaceList(goldList);
-  });
-  $('.silver-tier').click(() => {
-    $('.list_content').attr('data-current', 'silver');
-    replaceList(silverList);
-  });
-  $('.bronze-tier').click(() => {
-    $('.list_content').attr('data-current', 'bronze');
-    replaceList(bronzeList);
-  });
-  $('.sort_main').on('change', () => {
-    sortAndReplace();
-  });
-  $(document).on('click', '#editProfile', (elem) => {
-    handleModal($(elem.target).attr('data-user'));
-  });
-  $(document).on('click', '.btn', () => {
-    validation();
-    if (statusValid) {
-      $('.modal').css('display', 'none');
-      $('.page').css('filter', 'none');
-      $('.modal-input').val('');
-    }
-  });
+      console.log(errData);
+    },
+  );
+});
+
+$(document).on('click', '.click-me', (e) => {
+  handleModal($(e.target).attr('data-user'));
+});
+
+$('.title_item').click((e) => {
+  showGroups($(e.target).attr('data-group'));
+});
+
+$('.sort_main').on('change', () => {
+  sortList($('.sort-type:selected').attr('data-val'));
+});
+
+$(document).on('click', '.btn', () => {
+  validation();
+  if (statusValid) {
+    $('.modal').addClass('d-none');
+    $('.page').css('filter', 'none');
+    $('.modal-input').val('');
+  }
 });
